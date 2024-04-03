f66ab7a4d327872d5563c13f50552794fd8941b9
tcyang818 <tcyang818@gmail.com>
2024-04-03 17:01:19 +0800

    run in parallel

---
+++ tests/test_cal.py
@@ -0,0 +1,15 @@
+import pytest
+import logging
+
+def test_calculate():
+    assert 2*2 == 4
+
+def test_calculate_area_square_negative():
+    assert 2*2 > 0
+
+def test_calculate_area_square_string():
+    assert 2*2 == "4"
+
+def test_calculate_area_square_list():
+    assert 2*2 == ["4"]
+        
---
+++ tests/test_index.py
@@ -0,0 +1,19 @@
+import pytest
+import logging
+
+def test_001():
+    print(f"test log 001>>>>>>>")
+    assert 2 == 2
+
+def test_002():
+    print(f"test log 002>>>>>>>")
+    assert 2 == 2
+
+def test_003():
+    print(f"test log 003>>>>>>>")
+    assert 2 == 2
+
+def test_004():
+    print(f"test log 004>>>>>>>")
+    assert 2 +2 == 4
+        
